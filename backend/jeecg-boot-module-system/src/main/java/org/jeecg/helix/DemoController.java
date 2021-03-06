69156a956600e425f2b3a0edc212edcdd37269ed
TDL3 <29535697+TDL3@users.noreply.github.com>
2021-03-07 01:35:50 +0800

    jeecg bootstrap

---
+++ backend/jeecg-boot-module-system/src/main/java/org/jeecg/helix/DemoController.java
@@ -0,0 +1,25 @@
+package org.jeecg.helix;
+
+import lombok.extern.slf4j.Slf4j;
+import org.jeecg.common.api.vo.Result;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test/jeecgDemo")
+@Slf4j
+public class DemoController {
+    /**
+     * hello world
+     *
+     * @return res
+     */
+    @GetMapping(value = "/hello")
+    public Result<String> hello() {
+        Result<String> result = new Result<String>();
+        result.setResult("This is cool");
+        result.setSuccess(true);
+        return result;
+    }
+}
